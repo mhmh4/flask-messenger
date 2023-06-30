42e64cfe38c83dd4b8db001d041e257eba18c7f9
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-06-30 12:06:19 -0400

    Update __init__.py

--- rslh/__init__.py
+++ rslh/__init__.py
@@ -1,8 +1,21 @@
 import os
 
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "secret key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 
-from . import routes
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "signin"
+
+from rslh import routes
+from rslh import models
+
+with app.app_context():
+    db.create_all()
