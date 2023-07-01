b13bf00ae93d9d83a4fcbb6fd58a429ad92854ba
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-01 12:52:09 -0400

    Update routes.py

--- rslh/routes.py
+++ rslh/routes.py
@@ -1,9 +1,9 @@
 from flask import redirect, render_template, request, url_for
-from flask_login import login_user, logout_user
+from flask_login import current_user, login_required, login_user, logout_user
 
 from rslh import app, db
-from rslh.forms import LoginForm, RegistrationForm
-from rslh.models import User
+from rslh.forms import ConversationForm, LoginForm, RegistrationForm
+from rslh.models import Conversation, User
 
 
 @app.after_request
@@ -14,11 +14,15 @@ def after_request(response):
 
 @app.route("/", methods=["GET"])
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     return redirect(url_for("signin"))
 
 
 @app.route("/signin", methods=["GET", "POST"])
 def signin():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
         username = request.form.get("username")
@@ -33,6 +37,8 @@ def signin():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(
@@ -44,12 +50,24 @@ def signup():
     return render_template("signup.html", form=form)
 
 
-@app.route("/home", methods=["GET"])
+@app.route("/home", methods=["GET", "POST"])
+@login_required
 def home():
-    return render_template("home.html")
+    form = ConversationForm()
+    if request.method == "POST":
+        recipient = User.query.filter_by(username=request.form.get("username")).first()
+        if not recipient:
+            return redirect(url_for("home"))
+        db.session.add(Conversation(user_id=current_user.id))
+        db.session.add(Conversation(user_id=recipient.id))
+        db.session.commit()
+        return redirect(url_for("home"))
+    conversations = Conversation.query.filter_by(user_id=current_user.id).all()
+    return render_template("home.html", form=form, conversations=conversations)
 
 
 @app.route("/signout", methods=["GET"])
+@login_required
 def signout():
     logout_user()
     return redirect(url_for("signin"))
