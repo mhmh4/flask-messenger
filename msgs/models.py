fad9895eed5dd76503f33cd919bff5c5b4e0db46
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-02 16:01:08 -0400

    Update models.py

--- msgs/models.py
+++ msgs/models.py
@@ -16,12 +16,13 @@ class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    conversations = relationship("Conversation", backref="user", lazy=True)
+    # conversations = relationship("Conversation", backref="user", lazy=True)
 
 
 class Conversation(db.Model):
-    id = Column(Integer, primary_key=True)
+    uid = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.id"))
+    conversation_id = Column(Integer)
     messages = relationship("Message", backref="conversation", lazy=True)
 
 
@@ -29,5 +30,5 @@ class Message(db.Model):
     id = Column(Integer, primary_key=True)
     content = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow())
-    conversation_id = Column(Integer, ForeignKey("conversation.id"))
+    conversation_id = Column(Integer, ForeignKey("conversation.conversation_id"))
     user_id = Column(Integer, ForeignKey("user.id"))
